adbdeba80731eef858c1f50cf2c78f424f25a4a4
famiguel875 <famiguel@iesrafaelalberti.es>
2023-11-12 23:34:18 +0100

    Push 1.1

--- src/ejercicio_2_4_burbuja.py
+++ src/ejercicio_2_4_burbuja.py
@@ -1,4 +1,9 @@
 def ordenar_burbuja(lista):
+    """
+    @type ordenar_burbuja: función
+    @param lista: lista de números
+    @returns: lista de números ordenada de menor a mayor
+    """
     n = len(lista)
     for i in range(n - 1):
         for j in range(n - 1 - i):
